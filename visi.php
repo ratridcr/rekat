c2c675e85c4ba5d0eba206900873d9bfdfe27000
natanael kristiawan <natanaelkristiawan@hotmail.com>
2020-03-27 17:23:30 +0800

    revisi

--- visi.php
+++ visi.php
@@ -3,45 +3,58 @@
 
         <div class="swiper-slide">
             <div class="visi">
-                <h3 class="judul__main u-margin-bottom-small">Merekatkan, Menyatukan, dan Menyejukkan</h3>
-                <p class="judul__sub">Diinisiasi pada tanggal 1 oktober 2019.</p>
+                <div class="visi__box">
+                    <h3 class="judul__main u-margin-bottom-small">Merekatkan, Menyatukan, dan Menyejukkan</h3>
+                    <p class="judul__sub">Diinisiasi pada tanggal 1 oktober 2019.</p>
+                </div>
             </div>
         </div>
 
         <div class="swiper-slide">
             <div class="visi">
-                <h3 class="judul__main u-margin-bottom-small">Internalisasi dan Implementasi Pancasila</h3>
-                <p class="judul__sub">Untuk membangun karakter anak bangsa yang
-                    Pancasilais.</p>
+                <div class="visi__box">
+                    <h3 class="judul__main u-margin-bottom-small">Internalisasi dan Implementasi Pancasila</h3>
+                    <p class="judul__sub">Untuk membangun karakter anak bangsa yang
+                        Pancasilais.</p>
+                </div>
             </div>
         </div>
 
         <div class="swiper-slide">
             <div class="visi">
-                <h3 class="judul__main u-margin-bottom-small">Titik Temu</h3>
-                <p class="judul__sub">Semua pandangan dan nilai-nilai Keindonesiaan serta Nasionalisme.</p>
+                <div class="visi__box">
+                    <h3 class="judul__main u-margin-bottom-small">Titik Temu</h3>
+                    <p class="judul__sub">Semua pandangan dan nilai-nilai Keindonesiaan serta Nasionalisme.</p>
+                </div>
             </div>
         </div>
 
         <div class="swiper-slide">
             <div class="visi">
-                <h3 class="judul__main u-margin-bottom-small">Solusi</h3>
-                <p class="judul__sub">Keadilan Sosial dan Cinta Kasih yang mencerahkan.</p>
+                <div class="visi__box">
+                    <h3 class="judul__main u-margin-bottom-small">Solusi</h3>
+                    <p class="judul__sub">Keadilan Sosial dan Cinta Kasih yang mencerahkan.</p>
+                </div>
             </div>
         </div>
 
         <div class="swiper-slide">
             <div class="visi">
-                <h3 class="judul__main u-margin-bottom-small">Kemajuan manusia Indonesia</h3>
-                <p class="judul__sub">Dengan merawat tradisi, enjaga budaya, dan menghidupkan peradaban.</p>
+                <div class="visi__box">
+                    <h3 class="judul__main u-margin-bottom-small">Kemajuan manusia Indonesia</h3>
+                    <p class="judul__sub">Dengan merawat tradisi, enjaga budaya, dan menghidupkan peradaban.</p>
+                </div>
             </div>
         </div>
 
         <div class="swiper-slide">
             <div class="visi">
-                <h3 class="judul__main u-margin-bottom-small">Regenerasi total</h3>
-                <p class="judul__sub">Melalui pembaruan semangat persatuan indonesia, munasabah, dan dialog berbagai isu
-                    kebangsaan.</p>
+                <div class="visi__box">
+                    <h3 class="judul__main u-margin-bottom-small">Regenerasi total</h3>
+                    <p class="judul__sub">Melalui pembaruan semangat persatuan indonesia, munasabah, dan dialog berbagai
+                        isu
+                        kebangsaan.</p>
+                </div>
             </div>
         </div>
 
